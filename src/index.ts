c098286e04c66fbe36b69ea7f1bdfcf765f973b9
Makdoom Shaikh <makshaikh99@gmail.com>
2024-02-13 18:11:32 +0530

    feat: Initial commit node-ts starter pack

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+// Middlewares
+app.use(morgan('dev'));
+
+app.get('/health-check', (req, res) => {
+  return res.json({ success: true, message: 'OK' });
+});
+
+app.listen(8000, () => {
+  console.log('Server up and running at 8000');
+});
